83b9717f83c1cfc70c33e26253ebf9bddeb3e616
regiostech <thosakwe@gmail.com>
2016-05-31 23:23:59 -0400

    Still need: Optimizations...

---
+++ PathFinder/PathFinder/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PathFinder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> Directories = new List<string>();
+        List<string> Files = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void LoadPath()
+        {
+            Directories.Clear();
+            Files.Clear();
+            problems.Items.Clear();
+            results.Items.Clear();
+
+            string PATH = Environment.GetEnvironmentVariable("PATH");
+
+            foreach (string dir_ in PATH.Split(';'))
+            {
+                string dir = Regex.Replace(dir_.Trim(), "\\\\+$", "").Trim();
+
+                if (string.IsNullOrEmpty(dir))
+                    continue;
+
+                if (Directories.Contains(dir))
+                {
+                    problems.Items.Add($"Duplicate directory: {dir}");
+                }
+                else if (Directory.Exists(dir) && !Directories.Contains(dir))
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(dir);
+                    TreeViewItem root = new TreeViewItem();
+                    root.Header = dir;
+
+                    foreach (FileInfo program in directoryInfo.GetFilesByExtensions(
+                        // TODO: All executable extensions
+                        ".exe", ".bat", ".com", ".cmd", ".bin", ".cpl", ".gadget"
+                        ))
+                    {
+                        TreeViewItem child = new TreeViewItem();
+                        child.Header = program.Name;
+
+                        if (!Files.Contains(program.FullName))
+                        {
+                            Files.Add(program.FullName);
+                        }
+                        else
+                        {
+                            problems.Items.Add($"The same program is found in multiple entries: {program.FullName}");
+                        }
+
+                        root.Items.Add(child);
+                    }
+
+                    tvPrograms.Items.Add(root);
+                }
+                else
+                {
+                    problems.Items.Add($"Nonexistent directory in PATH: \"{dir}\"");
+                }
+            }
+        }
+
+        void Search()
+        {
+            string query = tbSearch.Text;
+
+            if (!string.IsNullOrEmpty(query))
+            {
+                results.Items.Clear();
+                tiSearch.Visibility = Visibility.Visible;
+
+                Regex matcher = new Regex(query.Trim().ToLower(), RegexOptions.IgnoreCase);
+                string[] matches = Files.Where(file =>
+                {
+                    return matcher.IsMatch(file);
+                }).ToArray();
+
+                foreach (string match in matches)
+                {
+                    results.Items.Add(match);
+                }
+
+                tabControl.SelectedItem = tiSearch;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoadPath();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadPath();
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Search();
+        }
+
+        private void tbSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Search();
+            }
+        }
+    }
+
+    public static class Ext
+    {
+        public static IEnumerable<FileInfo> GetFilesByExtensions(this DirectoryInfo dirInfo, params string[] extensions)
+        {
+            var allowedExtensions = new HashSet<string>(extensions, StringComparer.OrdinalIgnoreCase);
+
+            return dirInfo.GetFiles()
+                          .Where(f => allowedExtensions.Contains(f.Extension));
+        }
+    }
+}
---
+++ PathFinder/PathFinder/ProgramInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace PathFinder
+{
+    public class ProgramInfo
+    {
+        /// <summary>
+        /// The program's name.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// The program's absolute path.
+        /// </summary>
+        public string Path { get; private set; }
+
+        private BitmapImage icon_;
+
+        /// <summary>
+        /// An icon representing the program.
+        /// </summary>
+        public BitmapImage Icon
+        {
+            get
+            {
+                return icon_;
+            }
+        }
+
+        private ProgramInfo()
+        {
+
+        }
+
+        public static ProgramInfo FromFile(FileInfo file)
+        {
+            ProgramInfo programInfo = new ProgramInfo
+            {
+                Name = System.IO.Path.GetFileName(file.FullName),
+                Path = file.FullName
+            };
+
+            // TODO: Load icons
+
+            return programInfo;
+        }
+    }
+}
